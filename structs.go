4c60ecf76b6c32efd361f908964601b644db6575
Lee Brotherston <lee+github@squarelemon.com>
2019-03-08 20:40:29 -0500

    Make FingerprintFile exportable and remove unused structs

--- structs.go
+++ structs.go
@@ -1,26 +1,10 @@
 package dactyloscopy
 
-import (
-	"os"
-	"time"
-)
-
-type userConfig struct {
-	MinTLS    string   `json:"min_TLS_ver"`
-	Timeout   int64    `json:"timeout"`
-	AppLog    string   `json:"appLog"`
-	apFile    *os.File // Accompanying file descriptor
-	NewFPFile string   `json:"new_fingerprint_file"`
-	fpFile    *os.File // Accompanying file descriptor
-	EventLog  string   `json:"eventLog"`
-	eventFile *os.File // Accompanying file descriptor
-}
-
 // Type for json fingerprint in the fingerprints JSON file
 // Names much be uppercase to make this work and the `json:"name"`
 // maps them back to the names contained in the file for compatibility
 // with fingerprintls.
-type fingerprintFile struct {
+type FingerprintFile struct {
 	ID                float64 `json:"id"`
 	Desc              string  `json:"desc"`
 	RecordTLSVersion  string  `json:"record_tls_version"`
@@ -59,20 +43,4 @@ type FingerprintOutput struct {
 	FingerprintName string // The human readable name for the fingerprint, intended for log output, etc
 	Hostname        []byte // Destination hostname as taken from the SNI
 	Destination     []byte
-}
-
-// Event structs are used to express events via the API
-type Event struct {
-	//EventID    [32]string `json:"event_id"`		// Generated serverside
-	Event     string    `json:"event"`
-	FPHash    string    `json:"fp_hash,omitempty"`
-	IPVersion string    `json:"ip_version"`
-	IPDst     string    `json:"ipv4_dst"`
-	IPSrc     string    `json:"ipv4_src"`
-	SrcPort   uint16    `json:"src_port"`
-	DstPort   uint16    `json:"dst_port"`
-	TimeStamp time.Time `json:"timestamp"`
-	//	TLSVersion  uint16    `json:"tls_version"`  // Part of the fingerprint, doesn't need to be stored here
-	SNI string `json:"server_name"`
-	//Fingerprint `json:"fingerprint,omitempty"`
 }
