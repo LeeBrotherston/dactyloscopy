c29c9423555696ed983cbb7f7633d1cc21ebdcd3
Lee Brotherston <lee@squarelemon.com>
2022-08-31 23:09:55 -0400

    sorting out go.mod

--- go.mod
+++ go.mod
@@ -1,8 +1,5 @@
 module github.com/LeeBrotherston/dactyloscopy
 
-go 1.16
+go 1.18
 
-require (
-	github.com/spaolacci/murmur3 v1.1.0
-	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
-)
+require golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90
--- structs.go
+++ structs.go
@@ -2,21 +2,19 @@ package dactyloscopy
 
 import "golang.org/x/crypto/cryptobyte"
 
-// Fingerprint management... almost the same as fingerprintFile.  maybe
-// they can be combined in the future.
 type Fingerprint struct {
-	MessageType       uint8
-	RecordTLSVersion  uint8             //`json:"record_tls_version"`
-	TLSVersion        uint8             //`json:"tls_version"`
-	Ciphersuite       []uint16          //`json:"ciphersuite"`
-	Compression       cryptobyte.String //`json:"compression"`
-	Extensions        []uint16          //`json:"extensions"`
-	ECurves           cryptobyte.String //`json:"e_curves"`
-	SigAlg            cryptobyte.String //`json:"sig_alg"`
-	EcPointFmt        cryptobyte.String //`json:"ec_point_fmt"`
-	Grease            bool
-	SupportedVersions cryptobyte.String
-	LB1               string
-	JA3               string
-	SNI               cryptobyte.String
+	MessageType       uint8             `json:"message_type"`
+	RecordTLSVersion  uint8             `json:"record_tls_version"`
+	TLSVersion        uint8             `json:"tls_version"`
+	Ciphersuite       []uint16          `json:"ciphersuite"`
+	Compression       cryptobyte.String `json:"compression"`
+	Extensions        []uint16          `json:"extensions"`
+	ECurves           cryptobyte.String `json:"e_curves"`
+	SigAlg            cryptobyte.String `json:"sig_alg"`
+	EcPointFmt        cryptobyte.String `json:"ec_point_fmt"`
+	Grease            bool              `json:"grease"`
+	SupportedVersions cryptobyte.String `json:"supported_versions"`
+	LB1               string            `json:"lb1"`
+	JA3               string            `json:"ja3"`
+	SNI               cryptobyte.String `json:"sni"`
 }
